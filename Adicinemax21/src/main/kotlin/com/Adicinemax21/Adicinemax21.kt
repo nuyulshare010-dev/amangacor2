57120c1e9c45ef8262295d5b7728e69d9d464374
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-09 21:48:41 +0900

    Update Adicinemax21.kt

--- Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21.kt
+++ Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21.kt
@@ -120,7 +120,7 @@ open class Adicinemax21 : TmdbProvider() {
 
     private fun getImageUrl(link: String?): String? {
         if (link == null) return null
-        return if (link.startsWith("/")) "https://image.tmdb.org/t/p/w500/$link" else link
+        return if (link.startsWith("/")) "https://image.tmdb.org/t/p/original/$link" else link
     }
 
     private fun getOriImageUrl(link: String?): String? {
