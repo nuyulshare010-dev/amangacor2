52db3cab1f024d7dacb8b87907f91ed1a5c5a283
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-11 08:05:27 +0900

    Update JavHeyPlugin.kt

--- JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
+++ JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
@@ -10,63 +10,39 @@ class JavHeyPlugin: Plugin() {
         // 1. Daftarkan Provider Utama
         registerMainAPI(JavHey())
 
-        // 2. Daftarkan Semua Extractor Custom (Dari File Extractor.kt)
-        // Ini wajib agar CloudStream tahu cara menangani server-server ini
-        
-        // --- VidHide / FileLions ---
+        // 2. Daftarkan Extractor Sesuai Screenshot & Permintaan
+
+        // --- VidHidePro (Sesuai Screenshot) ---
         registerExtractorAPI(VidHidePro1())
         registerExtractorAPI(VidHidePro2())
         registerExtractorAPI(VidHidePro3())
         registerExtractorAPI(VidHidePro4())
         registerExtractorAPI(VidHidePro5())
         registerExtractorAPI(VidHidePro6())
+
+        // --- EarnVids (Sesuai Screenshot) ---
         registerExtractorAPI(Smoothpre())
         registerExtractorAPI(Dhtpre())
         registerExtractorAPI(Peytonepre())
 
-        // --- MixDrop ---
+        // --- MixDrop (Sesuai Screenshot) ---
         registerExtractorAPI(MixDropBz())
         registerExtractorAPI(MixDropAg())
         registerExtractorAPI(MixDropCh())
         registerExtractorAPI(MixDropTo())
 
-        // --- StreamWish ---
+        // --- Streamwish (Sesuai Screenshot) ---
         registerExtractorAPI(Mwish())
         registerExtractorAPI(Dwish())
-        registerExtractorAPI(Ewish())
-        registerExtractorAPI(WishembedPro())
-        registerExtractorAPI(Kswplayer())
-        registerExtractorAPI(Wishfast())
         registerExtractorAPI(Streamwish2())
-        registerExtractorAPI(SfastwishCom())
-        registerExtractorAPI(Strwish())
-        registerExtractorAPI(Strwish2())
-        registerExtractorAPI(FlaswishCom())
-        registerExtractorAPI(Awish())
-        registerExtractorAPI(Obeywish())
-        registerExtractorAPI(Jodwish())
-        registerExtractorAPI(Swhoi())
-        registerExtractorAPI(Multimovies())
-        registerExtractorAPI(UqloadsXyz())
-        registerExtractorAPI(Doodporn())
-        registerExtractorAPI(CdnwishCom())
-        registerExtractorAPI(Asnwish())
-        registerExtractorAPI(Nekowish())
-        registerExtractorAPI(Nekostream())
+        registerExtractorAPI(WishembedPro())
+        registerExtractorAPI(Wishfast())
+        
+        // --- Swdyu (Sesuai Screenshot) ---
         registerExtractorAPI(Swdyu())
-        registerExtractorAPI(Wishonly())
-        registerExtractorAPI(Playerwish())
-        registerExtractorAPI(StreamHLS())
-        registerExtractorAPI(HlsWish())
 
-        // --- Byse / Bysebuho ---
-        registerExtractorAPI(Bysezejataos())
-        registerExtractorAPI(ByseBuho())
-        registerExtractorAPI(ByseVepoin())
-
-        // --- DoodStream ---
+        // --- DoodStream (Sesuai Screenshot) ---
         registerExtractorAPI(D0000d())
-        registerExtractorAPI(D000dCom())
         registerExtractorAPI(DoodstreamCom())
         registerExtractorAPI(Dooood())
         registerExtractorAPI(DoodWfExtractor())
@@ -83,7 +59,7 @@ class JavHeyPlugin: Plugin() {
         registerExtractorAPI(Ds2video())
         registerExtractorAPI(MyVidPlay())
 
-        // --- LuluStream ---
+        // --- LuluStream (Request Tambahan) ---
         registerExtractorAPI(Lulustream1())
         registerExtractorAPI(Lulustream2())
     }
