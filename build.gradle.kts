f760a908aa08aa14c0bc4e90a8488795b370a802
nuyulshare010-dev <nuyulshare010@gmail.com>
2026-02-14 10:11:02 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,96 +1,77 @@
-import com.android.build.gradle.BaseExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.gradle.kotlin.dsl.register
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        // Repositori wajib agar Gradle bisa menemukan plugin Cloudstream
+        maven { url = uri("https://jitpack.io") }
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.2")
+        // Plugin utama untuk build plugin Cloudstream
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+        
+        // Plugin Kotlin untuk Android
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     }
 }
 
+// Menerapkan plugin yang diperlukan
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+// Konfigurasi untuk memastikan dependensi library juga dicari di JitPack
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/cloudstream-extensions-phisher")
-        authors = listOf("Phisher98")
+android {
+    // Sesuaikan dengan versi SDK yang Anda gunakan di project
+    compileSdk = 34
+    
+    defaultConfig {
+        minSdk = 21
     }
 
-    android {
-        namespace = "com.phisher98"
-
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
-                    "-Xannotation-default-target=param-property"
-                )
-            }
-        }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    dependencies {
-        val implementation by configurations
-        val cloudstream by configurations
-        
-        // KEMBALI KE VERSI LAMA (Biar file lain gak error)
-        cloudstream("com.lagradost:cloudstream3:pre-release") 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 
-        // Other dependencies
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.16")
-        implementation("org.jsoup:jsoup:1.22.1")
-        implementation("androidx.annotation:annotation:1.9.1")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-        implementation("org.mozilla:rhino:1.9.0")
-        implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.13.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-        implementation("com.github.vidstige:jadb:v1.2.1")
-        implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/java")
+            manifest.srcFile("src/main/AndroidManifest.xml")
+        }
     }
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+// Konfigurasi khusus Cloudstream
+configure<CloudstreamExtension> {
+    // Nama file hasil build .cs3 nantinya
+    // Jika Anda ingin nama spesifik, ganti di sini
+}
+
+dependencies {
+    val cloudstreamVersion = "pre-release" // atau versi spesifik lainnya
+    compileOnly("com.github.recloudstream:cloudstream:$cloudstreamVersion")
+    
+    // Tambahkan dependensi lain yang diperlukan plugin Anda di sini
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs = freeCompilerArgs + "-Xjvm-default=all"
+    }
 }
