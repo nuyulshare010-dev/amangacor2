3d472ad5bd309fae53ce8018c66765caa0718eae
nuyulshare010-dev <nuyulshare010@gmail.com>
2026-02-14 10:21:17 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,20 @@
-rootProject.name = "CloudstreamPlugins"
-
-// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
-val disabled = listOf<String>()
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url 'https://jitpack.io' } // Tambahkan ini agar plugin ditemukan
     }
 }
 
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url 'https://jitpack.io' } // Tambahkan ini untuk library
+    }
 }
 
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+rootProject.name = "CloudstreamPlugins"
+include ':app' // Sesuaikan dengan nama modul Anda, biasanya ':app' atau ':plugin'
