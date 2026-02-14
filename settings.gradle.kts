48794c313784e3fe277a05a4e30061f87246a81c
nuyulshare010-dev <nuyulshare010@gmail.com>
2026-02-14 10:49:13 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,11 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        // Menggunakan sintaks Kotlin DSL yang benar
         maven { url = uri("https://jitpack.io") }
     }
 }
 
 dependencyResolutionManagement {
-    // Memastikan repositori ini digunakan oleh semua modul aplikasi
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
@@ -19,7 +17,3 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "CloudstreamPlugins"
-
-// Pastikan folder plugin Anda terdaftar di sini
-// Jika foldernya bernama 'app', gunakan:
-include(":app")
